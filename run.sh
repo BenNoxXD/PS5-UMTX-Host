14e7b4d7ab39935170e939af0514c7adee65aea9
BenNoxXD <90966148+BenNoxXD@users.noreply.github.com>
2024-09-30 21:38:39 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 cd /home/$USER/PS5-UMTX-Jailbreak-main/
-sudo python3 host.py & sudo python3 fakedns.py -c dns.conf -i 192.168.50.84
+sudo python3 host.py & sudo python3 fakedns.py -c dns.conf -i 192.168.94.132
