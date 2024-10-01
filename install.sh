21ab866d7ac97f4eb2bd0b276e1653fc82f6d0ea
BenNoxXD <90966148+BenNoxXD@users.noreply.github.com>
2024-10-01 07:42:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
-chmod 777 run.sh
 sudo apt-get install git -y
 sudo apt-get indtall python -y 
+sudo rm -r /home/$USER/PS5-UMTX-Host/
+git clone https://github.com/BenNoxXD/PS5-UMTX-Host/
+cd PS5-UMTX-Host
 git clone https://github.com/PS5Dev/PS5-UMTX-Jailbreak
 cd PS5-UMTX-Jailbreak
 truncate -s 0 dns.conf
 cd ..
+chmod 777 run.sh
 ./run.sh
